ea3dfd9ca1fb33c086dad3cb804a74d75ede27ca
WALTER DAVID IRUNGU <daveirungu20@gmail.com>
2024-08-02 04:24:03 +0300

    COFFEE UPDATE

--- src/components/Article.js
+++ src/components/Article.js
@@ -1,10 +1,23 @@
 import React from "react";
 
-function Article({title,date="January 1, 1970",preview}){
+
+function ReadingTime({minutes}){
+    if (minutes <30){
+        const cups=Math.ceil(minutes/5)
+        return "☕️".repeat(cups) + `${minutes} min read`
+    }
+    else{
+        const bentos = Math.ceil(minutes / 10);
+        return "🍱".repeat(bentos) + ` ${minutes} min read`;
+    }
+}
+
+function Article({title,date="January 1, 1970",preview,minutes}){
     return(
         <article>
             <h3>{title}</h3>
             <small>{date}</small>
+            <small>.                             <ReadingTime minutes={minutes}/></small>
             <p>{preview}</p>
         </article>
     )
